26bcfb27189595c33c66e9346f0b21198a842be9
jose <jose12_bernal@hotmail.com>
2024-03-12 15:26:23 -0600

    co

--- dist/services/products.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateProduct = exports.deleteProduct = exports.getProductsById = exports.getProducts = exports.addProduct = void 0;
-exports.addProduct = () => { };
-exports.getProducts = () => { };
-exports.getProductsById = () => { };
-exports.deleteProduct = () => { };
-exports.updateProduct = () => { };
